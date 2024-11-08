1a869f613bfc904159926a1d455e86b1b3cc34ae
Baja The Frog <bajathefrog@gmail.com>
2024-11-07 23:12:21 -0500

    Added missing space

--- icon_man.gd
+++ icon_man.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED =800.0
+const SPEED = 800.0
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
